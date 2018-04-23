1d90b643afe6b3d2df1ea2ec8d901acb7d945035
MakanWG <pkoulibaly@infinitesquare.com>
2018-04-23 18:59:51 +0200

    started design integration

--- app/index.ts
+++ app/index.ts
@@ -2,27 +2,29 @@
 import clock from "clock";
 import document from "document";
 import { preferences } from "user-settings";
+import {locale} from "user-settings";
 import * as util from "../common/utils";
 
 // Update the clock every minute
 clock.granularity = "minutes";
 
 // Get a handle on the <text> element
-const myLabel = document.getElementById("myLabel");
+const displayedHours = document.getElementById("displayedHours");
+const displayedMinutes = document.getElementById("displayedMinutes");
+const displayedDate = document.getElementById("displayedDate");
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt :any) => {
   let today = evt.date;
   let hours: number = today.getHours();
-  let displayedHours: string;
-  let displayedMinutes: string;
   if (preferences.clockDisplay === "12h") {
     // 12h format
-    displayedHours = `${hours % 12 || 12}`;
+    displayedHours.text = `${hours % 12 || 12}`;
   } else {
     // 24h format
-    displayedHours = util.zeroPad(hours);
+    displayedHours.text = util.zeroPad(hours);
   }
-  displayedMinutes = util.zeroPad(today.getMinutes());
-  myLabel.text = `${displayedHours}:${displayedMinutes}`;
+  displayedMinutes.text = util.zeroPad(today.getMinutes());
+  //displayedDate.text = `${today.getDate()}-${(today.getMonth() + 1)}-${today.getFullYear()}`;
+  displayedDate.text = today.toLocaleString(locale.language, { year:"", month: "short" });
 }
